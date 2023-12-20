c87365d616879866944d717c15788f5f5fcc131c
nikolasfil <nikolas13in@gmail.com>
2023-12-20 04:07:03 +0200

    routes

---
+++ routes/route_homepage.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+const database = require('../controllers/database.js');
+const login = require('../controllers/login.js');
+
+
+/**
+ * Router for homepage
+ */
+router.get('/',
+    (req, res, next) => {
+        // this is a static book list displaying 12 books
+        // database.getAllDevices(id=null, serial=null, battery=null, status='active', type='tracker', function (err, devices) {
+            database.getAllDevices(id=null, serial=null, battery=null, 
+                status="active", type="tracker", limit=5, offset = null,
+                function (err, devices) {
+            if (err) {
+                console.log(err)
+                res.status(500).send('Internal Server Error')
+            } else {
+                res.locals.active_trackers = devices;
+                next();
+            }
+        })
+        
+    },
+    
+    (req, res) => {
+        res.render('homepage', {
+            style: 'index.css',
+            title: 'Home',
+            signedIn: req.session.signedIn,
+        });
+    });
+
+
+
+router.get('/homepage', (req, res) => {
+    res.redirect('/');
+});
+
+
+module.exports = router;
+
+
---
+++ routes/route_user_profile.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const login = require('../controllers/login.js')
+
+const database = require('../controllers/database.js');
+
+const router = express.Router();
+
+
+router.get('/user_profile', login.checkAuthentication,
+    (req, res, next) => {
+        database.userDetails(req.session.userid, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.locals.profile = result;
+                next();
+            }
+        });
+    },
+    
+    (req, res) => { 
+        res.render('user_profile', {
+            title: 'User Profile',
+            style: 'user_profile.css',
+            signedIn: req.session.signedIn
+        });
+    }
+);
+
+
+module.exports = router;
+
+
