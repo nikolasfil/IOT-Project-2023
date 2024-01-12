9d0c1da48ee44dd935c37070390aa324a6450618
nikolasfil <nikolas13in@gmail.com>
2024-01-12 21:52:32 +0200

    updating database

--- website/model/database_managing.py
+++ website/model/database_managing.py
@@ -6,6 +6,12 @@ from database import Database
 class AdventureGuard(Database):
     def __init__(self, database, sqlfile):
         super().__init__(database, sqlfile)
+        self.num = 100
+        self.users = []
+        self.devices = []
+        self.assigned = []
+        self.pressed = []
+        self.tracked = []
 
     def main(self):
         """main function"""
@@ -16,9 +22,13 @@ class AdventureGuard(Database):
     def fill_database(self):
         self.clear_all()
         self.fill_users()
+        print("Filled users table")
         self.fill_device()
+        print("Filled device table")
         self.fill_assigned()
+        print("Filled assigned table")
         self.fill_pressed()
+
         self.fill_tracked()
 
     def fill_users(self):
@@ -30,7 +40,6 @@ class AdventureGuard(Database):
         self.clearing(tableName)
 
         file = self.path_to_file("users.csv")
-        num = 100
         headers = []
         with open(file, "r") as f:
             for i, line in enumerate(f):
@@ -38,7 +47,7 @@ class AdventureGuard(Database):
                 if i == 0:
                     headers = line[:]
                     continue
-                elif i > num:
+                elif i > self.num:
                     break
 
                 data = {}
@@ -51,6 +60,8 @@ class AdventureGuard(Database):
                 self.insert_data(
                     tableName, [data[col] for col in self.tables[tableName]]
                 )
+                self.users.append(data)
+
         print("Filled users table")
 
     def fill_device(self):
@@ -62,7 +73,6 @@ class AdventureGuard(Database):
         self.clearing(tableName)
 
         file = self.path_to_file("device.csv")
-        num = 100
         headers = []
         with open(file, "r") as f:
             for i, line in enumerate(f):
@@ -70,7 +80,7 @@ class AdventureGuard(Database):
                 if i == 0:
                     headers = line[:]
                     continue
-                elif i > num:
+                elif i > self.num:
                     break
 
                 data = {}
@@ -81,14 +91,43 @@ class AdventureGuard(Database):
                     tableName, [data[col] for col in self.tables[tableName]]
                 )
 
+                self.devices.append(data)
+
     def fill_assigned(self):
-        pass
+        tableName = "Assigned"
+        self.clearing(tableName)
+
+        # user id
+        # device id
+        # date_received
+        # date_returned
+
+        for i in range(self.num):
+            user_id = self.users[i]["id"]
+            device_id = self.devices[i]["id"]
+            date_received = self.random_date()
+            # date_returned is after date_received
+            date_returned = self.random_date(date_received)
+
+            data = [user_id, device_id, date_received, date_returned]
+            self.insert_data(tableName, data)
+            self.assigned.append(data)
 
     def fill_pressed(self):
         pass
 
     def fill_tracked(self):
         pass
+
+    def random_date(self, start="1/1/2023", end="1/1/2024"):
+        """Generate a random date between two dates"""
+        from datetime import datetime
+        import random
+
+        start = datetime.strptime(start, "%m/%d/%Y")
+        end = datetime.strptime(end, "%m/%d/%Y")
+
+        return start + (end - start) * random.random()
 
 
 if __name__ == "__main__":
