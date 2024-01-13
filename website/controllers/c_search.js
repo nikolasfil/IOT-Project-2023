42767d696f781c67877193d91bedf20490566b3d
nikolasfil <nikolas13in@gmail.com>
2024-01-13 02:11:27 +0200

    cleaned a tiny bit of the code

--- website/controllers/c_search.js
+++ website/controllers/c_search.js
@@ -1,6 +1,7 @@
 
 exports.filtering=(req, res, next) =>{
     let filters = JSON.stringify(req.body.filters);
+    
         
     // throw out all the keys in the json that don't they have an empty list as value
     filters = JSON.parse(filters)
@@ -9,33 +10,31 @@ exports.filtering=(req, res, next) =>{
             delete filters[key]
         }
     }
+
+    let data = {};
+
+    if (filters.assigned && filters.assigned.includes('Assigned') ){
+        console.log(filters);
+        data.u_id = req.body.searchValue;
+        data.assigned = true;
+    } else if (filters.assigned && filters.assigned.includes('Unassigned') ){
+        data.assigned = false;
+    }
+
+
     res.locals.filters = filters;
+    res.locals.data = data;
     next();
 }
 
 
 exports.data_minining = (req, res, next) => {
-        let data ={};
-        let filters = res.locals.filters;
+        let data = res.locals.data;
+
+        data.filters = res.locals.filters;
 
         data.limit = req.body.limit;
         data.offset = req.body.offset;
-
-        if (filters.assigned && filters.assigned.includes('Assigned') ){
-            console.log(filters);
-            data.u_id = req.body.searchValue;
-            data.assigned = true;
-            // delete filters.assigned;
-        } else if (filters.assigned && filters.assigned.includes('Unassigned') ){
-            data.assigned = false;
-            // delete filters.assigned;
-        }
-        // else { 
-            // data.assigned = false;
-            // delete filters.assigned;
-        // }
-
-        data.filters = filters;
         
         data.serial = req.body.searchValue;
         data.d_id = req.body.searchValue;
