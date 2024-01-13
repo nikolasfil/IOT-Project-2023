5379251c9fcfef09e853f0b1882420eb9ba2eefe
nikolasfil <nikolas13in@gmail.com>
2024-01-14 00:05:30 +0200

    update datbase functions

--- website/controllers/c_search.js
+++ website/controllers/c_search.js
@@ -1,52 +1,5 @@
 
-// exports.filtering=(req, res, next) =>{
-//     let filters = JSON.stringify(req.body.filters);
-    
-        
-//     // throw out all the keys in the json that don't they have an empty list as value
-//     filters = JSON.parse(filters)
-//     for (let key in filters) {
-//         if (filters[key].length == 0) {
-//             delete filters[key]
-//         }
-//     }
-
-//     let data = {};
-
-//     if (filters.assigned && filters.assigned.includes('Assigned') ){
-//         console.log(filters);
-//         data.u_id = req.body.searchValue;
-//         data.assigned = true;
-//     } else if (filters.assigned && filters.assigned.includes('Unassigned') ){
-//         data.assigned = false;
-//     }
-
-//     res.locals.filters = filters;
-//     res.locals.data = data;
-//     next();
-// }
-
-
-// exports.data_minining = (req, res, next) => {
-//         let data = res.locals.data;
-
-//         data.filters = res.locals.filters;
-
-//         data.limit = req.body.limit;
-//         data.offset = req.body.offset;
-        
-//         data.serial = req.body.searchValue;
-//         data.d_id = req.body.searchValue;
-
-//         data.exclusively = req.body.exclusively;
-//         data.regex = req.body.regex;
-
-//         res.locals.data = data;
-//         next();
-// }
-
-
-function filtering(req, res, next) {
+exports.filtering=(req, res, next) =>{
     let filters = JSON.stringify(req.body.filters);
     
         
@@ -74,7 +27,7 @@ function filtering(req, res, next) {
 }
 
 
-function data_minining (req, res, next) {
+exports.data_minining = (req, res, next) => {
         let data = res.locals.data;
 
         data.filters = res.locals.filters;
@@ -92,7 +45,3 @@ function data_minining (req, res, next) {
         next();
 }
 
-module.exports = {
-    filtering: filtering,
-    data_minining: data_minining
-};
--- website/controllers/database.js
+++ website/controllers/database.js
@@ -44,310 +44,310 @@ function getRegex(searchValue, rows) {
 
 
 
-module.exports = {
+// module.exports = {
 
-    addingActivated: function(activated_name, linker, regex) {
-        let query_activated ;
-        
-        console.log(regex)
-        if (!regex){
-            query_activated = activated_name.map((name) => `${name} = ?`).join(linker)
+exports.addingActivated=(activated_name, linker, regex)=> {
+    let query_activated ;
+    
+    console.log(regex)
+    if (!regex){
+        query_activated = activated_name.map((name) => `${name} = ?`).join(linker)
+    }
+    else {
+        let searchable = [] ;
+
+        this.getAllDevicesJson(data={linker:'or', regex:false}, function(err, rows) {
+            searchable.push(rows.map(row => row.serial));
+            searchable.push(rows.map(row => row.d_id));
+            // searchable.push(rows.map(row => row.user));
+        })
+        query_activated = activated_name.map(
+            (name) => `${name} in (${getRegex(name, searchable).map(word => `'${word}'`).join(',')})`
+            ).join(linker)
+    }    
+    return query_activated
+}
+
+/**
+ * Returns information about the device . Every option other than callback is optional , if no option is given it will return all the devices
+ * @param {*} data contains everything in a json format (not optional)
+ * @param {*} id For a specific device (optional)
+ * @param {*} serial For a specific serial (optional)
+ * @param {*} battery For a specific battery (optional)
+ * @param {*} status For a specific status (optional)
+ * @param {*} type For a specific type (optional)
+ * @param {*} limit Limiting the number of results (optional)
+ * @param {*} offset Starting from a specific result (optional)
+ * @param {*} numOf true or null, if we want to focus more on the number of results back (optional)
+ * @param {*} filters Filters that are applied (optional), json of the form {key: [value1, value2, ...]}
+ * @param {*} callback function that handles the results
+ *  
+ * This function is the same as getAllDevices but it returns the results in a json format
+*/
+exports.getAllDevicesJson= (data,  callback) =>  {
+    
+
+    // Defining the variables
+    let stmt, device, query, query_filters, query_activated, linker;
+    
+    // initializing some variables
+    query_filters = "";
+    query_activated = "";
+    query_unassigned = "";
+    
+    // ----------- initializing arguments -----------
+
+    // Assigning value to the linker
+    if (!data.exclusively) {
+        linker = ' or '
+    }else {
+        linker = ' and '
+    }
+
+    // Working on the arguments provided
+    let activated = []; 
+    let activated_name = [];
+
+    // List of arguments that are not iterated
+    let non_iterated = ['filters', 'limit', 'offset', 'numOf','exclusively', 'linker','regex','assigned']
+
+
+
+
+    // ----------- Building the list of activated arguments ----------- 
+
+    // Iterate through the data json and add to the activated list the arguments that are activated and to the activated_name the key of that 
+    for (let key in data) {
+        // Get all the data that are not null and are not in the not iterate list 
+        if (data[key] && !non_iterated.includes(key)) {
+            activated.push(data[key])
+            activated_name.push(key)
+        }
+    }
+
+
+
+    // ----------- Building the query -----------
+
+    query = ` Select `
+
+    if (data.numOf ) {
+        // If the numOf is true then it will return the number of results
+        query += ` COUNT(*) as count_result`
+    }
+    
+    if (data.numOf && data.assigned) {
+        query += ` , `
+    } else if (!data.numOf && !data.assigned) {
+        query += ` * `
+    } 
+
+    if (data.assigned){
+        // Else it will return all the fields 
+        query += ` d_id , serial, battery, status, type, u_id, first_name, last_name, phone, date_received, date_returned `
+    } 
+
+
+    // Add the table name
+    query += ` FROM DEVICE `
+    
+    if (data.assigned) {
+        query += ` JOIN Assigned on d_id = device_id JOIN USER on user_id = u_id`
+    } else if (data.assigned === false ){ 
+        query_unassigned = `  d_id NOT IN (SELECT device_id FROM Assigned) `
+    }
+
+
+    // ----------- Building the activated arguments -----------
+
+    
+    // query_activated = activated_name.map((name) => `${name} = ?`).join(linker)
+    query_activated = this.addingActivated(activated_name, linker, data.regex)
+    
+
+    // ----------- Building the filters -----------
+
+    let filters = data.filters;
+
+    if (filters) {
+        // Filters for the filters that have a value, changed into the format of key in (values) and joins them in an and 
+        query_filters = Object.entries(filters)
+        // .filter(([key, value]) => value.length || key !== 'assigned')
+        .filter(([key, value]) => value.length && key !== 'assigned')
+        .map(([key, value]) => `${key} in (${value.map(word => `'${word}'`).join(',')})`)
+        .join(' and ');
+    }
+    
+    // ----------- Building the final query -----------
+    if ( query_activated.length || query_filters.length  || query_unassigned.length) {
+        query += ` WHERE `
+    }
+
+    // Add the activated arguments to the query
+    query += query_activated
+
+    if ( query_activated.length && query_filters.length ) {
+        query += ` and `
+    }
+
+    // Add the filter arguments to the query 
+    query += query_filters
+
+    if ( (query_activated.length && query_unassigned.length) || (query_filters.length && query_unassigned.length)) {
+        query += ` and `
+    }
+
+    // Add the unassigned argument to the query
+    query += query_unassigned
+
+
+    // Add the limit and offset to the query
+    if (data.limit) {
+        query += ' LIMIT ? '
+        activated.push(data.limit)
+    }
+
+    if (data.offset) {
+        query += ' OFFSET ? '
+        activated.push(data.offset)
+    }
+
+    // ----------- Printing the final query -----------
+    console.log(query)
+
+    try {
+        stmt = betterDb.prepare(query)
+        if (activated.length) {
+            device = stmt.all(activated);
         }
         else {
-            let searchable = [] ;
-    
-            this.getAllDevicesJson(data={linker:'or', regex:false}, function(err, rows) {
-                searchable.push(rows.map(row => row.serial));
-                searchable.push(rows.map(row => row.d_id));
-                // searchable.push(rows.map(row => row.user));
-            })
-            query_activated = activated_name.map(
-                (name) => `${name} in (${getRegex(name, searchable).map(word => `'${word}'`).join(',')})`
-                ).join(linker)
-        }    
-        return query_activated
-    },
-    
-    /**
-     * Returns information about the device . Every option other than callback is optional , if no option is given it will return all the devices
-     * @param {*} data contains everything in a json format (not optional)
-     * @param {*} id For a specific device (optional)
-     * @param {*} serial For a specific serial (optional)
-     * @param {*} battery For a specific battery (optional)
-     * @param {*} status For a specific status (optional)
-     * @param {*} type For a specific type (optional)
-     * @param {*} limit Limiting the number of results (optional)
-     * @param {*} offset Starting from a specific result (optional)
-     * @param {*} numOf true or null, if we want to focus more on the number of results back (optional)
-     * @param {*} filters Filters that are applied (optional), json of the form {key: [value1, value2, ...]}
-     * @param {*} callback function that handles the results
-     *  
-     * This function is the same as getAllDevices but it returns the results in a json format
-    */
-    getAllDevicesJson: function (data,  callback) {
-        
-
-        // Defining the variables
-        let stmt, device, query, query_filters, query_activated, linker;
-        
-        // initializing some variables
-        query_filters = "";
-        query_activated = "";
-        query_unassigned = "";
-        
-        // ----------- initializing arguments -----------
-
-        // Assigning value to the linker
-        if (!data.exclusively) {
-            linker = ' or '
-        }else {
-            linker = ' and '
+            device = stmt.all();
         }
 
-        // Working on the arguments provided
-        let activated = []; 
-        let activated_name = [];
-
-        // List of arguments that are not iterated
-        let non_iterated = ['filters', 'limit', 'offset', 'numOf','exclusively', 'linker','regex','assigned']
-
-
-
-
-        // ----------- Building the list of activated arguments ----------- 
-
-        // Iterate through the data json and add to the activated list the arguments that are activated and to the activated_name the key of that 
-        for (let key in data) {
-            // Get all the data that are not null and are not in the not iterate list 
-            if (data[key] && !non_iterated.includes(key)) {
-                activated.push(data[key])
-                activated_name.push(key)
-            }
-        }
-
-
-
-        // ----------- Building the query -----------
-
-        query = ` Select `
-
-        if (data.numOf ) {
-            // If the numOf is true then it will return the number of results
-            query += ` COUNT(*) as count_result`
-        }
-        
-        if (data.numOf && data.assigned) {
-            query += ` , `
-        } else if (!data.numOf && !data.assigned) {
-            query += ` * `
-        } 
-
-        if (data.assigned){
-            // Else it will return all the fields 
-            query += ` d_id , serial, battery, status, type, u_id, first_name, last_name, phone, date_received, date_returned `
-        } 
-
-
-        // Add the table name
-        query += ` FROM DEVICE `
-        
-        if (data.assigned) {
-            query += ` JOIN Assigned on d_id = device_id JOIN USER on user_id = u_id`
-        } else if (data.assigned === false ){ 
-            query_unassigned = `  d_id NOT IN (SELECT device_id FROM Assigned) `
-        }
-
-
-        // ----------- Building the activated arguments -----------
-
-        
-        // query_activated = activated_name.map((name) => `${name} = ?`).join(linker)
-        query_activated = this.addingActivated(activated_name, linker, data.regex)
-        
-
-        // ----------- Building the filters -----------
-
-        let filters = data.filters;
-
-        if (filters) {
-            // Filters for the filters that have a value, changed into the format of key in (values) and joins them in an and 
-            query_filters = Object.entries(filters)
-            // .filter(([key, value]) => value.length || key !== 'assigned')
-            .filter(([key, value]) => value.length && key !== 'assigned')
-            .map(([key, value]) => `${key} in (${value.map(word => `'${word}'`).join(',')})`)
-            .join(' and ');
-        }
-        
-        // ----------- Building the final query -----------
-        if ( query_activated.length || query_filters.length  || query_unassigned.length) {
-            query += ` WHERE `
-        }
-
-        // Add the activated arguments to the query
-        query += query_activated
-
-        if ( query_activated.length && query_filters.length ) {
-            query += ` and `
-        }
-
-        // Add the filter arguments to the query 
-        query += query_filters
-
-        if ( (query_activated.length && query_unassigned.length) || (query_filters.length && query_unassigned.length)) {
-            query += ` and `
-        }
-
-        // Add the unassigned argument to the query
-        query += query_unassigned
-
-
-        // Add the limit and offset to the query
-        if (data.limit) {
-            query += ' LIMIT ? '
-            activated.push(data.limit)
-        }
-
-        if (data.offset) {
-            query += ' OFFSET ? '
-            activated.push(data.offset)
-        }
-
-        // ----------- Printing the final query -----------
+    } catch (err) {
         console.log(query)
+        callback(err, null)
+    }
+    callback(null, device);
 
-        try {
-            stmt = betterDb.prepare(query)
-            if (activated.length) {
-                device = stmt.all(activated);
-            }
-            else {
-                device = stmt.all();
-            }
-
-        } catch (err) {
-            console.log(query)
-            callback(err, null)
-        }
-        callback(null, device);
-
-
-    },
-
-    select: function(command, callback) {
-        let stmt, result;
-        try {
-            stmt = betterDb.prepare(command)
-            result = stmt.all();
-        } catch (err) {
-            callback(err, null)
-        }
-        callback(null, result);
-    },
-
-
-    getAllAtributes: function(source,attribute, limit, offset, callback) {
-        let stmt, result;
-        let query = `Select distinct`
-
-        let list = []
-
-        query += ` ${attribute} as name from ${source}`
-
-        query += ` where ${attribute} is not null` 
-        query += ` order by ${attribute} asc`
-
-        if (limit) {
-            query += ' LIMIT ?'
-            list.push(limit)
-        }
-
-        // if (offset) {
-        //     query += ' OFFSET ?'
-        //     list.push(offset)
-        // }
-        
-
-        try {
-            stmt = betterDb.prepare(query)
-            result = stmt.all(list);
-        } catch (err) {
-            callback(err, null)
-        }
-        callback(null, result);
-    },
-
-
-    checkIfUserExists: function (id, callback) {
-        const stmt = betterDb.prepare('Select * from USER where u_id = ? ')
-        let user;
-        try {
-            user = stmt.get(id)
-            callback(null, user)
-        }
-        catch (err) {
-            callback(err, null)
-        }
-    },
-
-    userDetails: function (id, callback) {
-        // let query = `Select u_id, first_name, last_name, phone,role from USER where u_id = ? `
-        let query = `Select * from USER where u_id = ? `
-        // const stmt = betterDb.prepare('Select u_id, first_name, last_name, phone,role from USER where u_id = ? ')
-        const stmt = betterDb.prepare(query)
-        let user;
-        try {
-            user = stmt.get(id)
-            // console.log(id)
-            callback(null, user)
-        }
-        catch (err) {
-            callback(err, null)
-        }
-    },
-
-    checkUser: function (id, password, callback) {
-
-        let user, error_message;
-
-        try {
-            const stmt = betterDb.prepare('Select * from USER where u_id = ?')
-            user = stmt.get(id)
-            if (user) {
-                const match = bcrypt.compareSync(password, user.password);
-                if (match) {
-                    callback(null, user)
-                }
-                else {
-
-                    callback('Wrong Password', null)
-                }
-            } else {
-                callback('User not found', null)
-            }
-        }
-        catch (err) {
-            callback(err, null)
-        }
-    },
-
-    addUser: function (user, callback) {
-        let attributes = [user.first_name, user.last_name, user.phone, user.role, bcrypt.hashSync(user.psw, 10)]
-        let attibutes_name = ['first_name', 'last_name', 'phone', 'role', 'password']
-        
-        let query = `Insert into USER (${attibutes_name.join(',')}) values (${attibutes_name.map(() => '?').join(', ')})`
-        const stmt = betterDb.prepare(query)
-
-        try {
-            stmt.run(attributes)
-            callback(null, true)
-        }
-        catch (err) {
-            callback(err, null)
-        }
-    },
-
-
-
-    
 
 }
+
+exports.select=(command, callback) =>  {
+    let stmt, result;
+    try {
+        stmt = betterDb.prepare(command)
+        result = stmt.all();
+    } catch (err) {
+        callback(err, null)
+    }
+    callback(null, result);
+}
+
+
+exports.getAllAtributes=(source,attribute, limit, offset, callback) =>  {
+    let stmt, result;
+    let query = `Select distinct`
+
+    let list = []
+
+    query += ` ${attribute} as name from ${source}`
+
+    query += ` where ${attribute} is not null` 
+    query += ` order by ${attribute} asc`
+
+    if (limit) {
+        query += ' LIMIT ?'
+        list.push(limit)
+    }
+
+    // if (offset) {
+    //     query += ' OFFSET ?'
+    //     list.push(offset)
+    // }
+    
+
+    try {
+        stmt = betterDb.prepare(query)
+        result = stmt.all(list);
+    } catch (err) {
+        callback(err, null)
+    }
+    callback(null, result);
+}
+
+
+exports.checkIfUserExists= (id, callback) =>  {
+    const stmt = betterDb.prepare('Select * from USER where u_id = ? ')
+    let user;
+    try {
+        user = stmt.get(id)
+        callback(null, user)
+    }
+    catch (err) {
+        callback(err, null)
+    }
+}
+
+exports.userDetails= (id, callback) =>  {
+    // let query = `Select u_id, first_name, last_name, phone,role from USER where u_id = ? `
+    let query = `Select * from USER where u_id = ? `
+    // const stmt = betterDb.prepare('Select u_id, first_name, last_name, phone,role from USER where u_id = ? ')
+    const stmt = betterDb.prepare(query)
+    let user;
+    try {
+        user = stmt.get(id)
+        // console.log(id)
+        callback(null, user)
+    }
+    catch (err) {
+        callback(err, null)
+    }
+}
+
+exports.checkUser= (id, password, callback) =>  {
+
+    let user, error_message;
+
+    try {
+        const stmt = betterDb.prepare('Select * from USER where u_id = ?')
+        user = stmt.get(id)
+        if (user) {
+            const match = bcrypt.compareSync(password, user.password);
+            if (match) {
+                callback(null, user)
+            }
+            else {
+
+                callback('Wrong Password', null)
+            }
+        } else {
+            callback('User not found', null)
+        }
+    }
+    catch (err) {
+        callback(err, null)
+    }
+}
+
+exports.addUser= (user, callback) =>  {
+    let attributes = [user.first_name, user.last_name, user.phone, user.role, bcrypt.hashSync(user.psw, 10)]
+    let attibutes_name = ['first_name', 'last_name', 'phone', 'role', 'password']
+    
+    let query = `Insert into USER (${attibutes_name.join(',')}) values (${attibutes_name.map(() => '?').join(', ')})`
+    const stmt = betterDb.prepare(query)
+
+    try {
+        stmt.run(attributes)
+        callback(null, true)
+    }
+    catch (err) {
+        callback(err, null)
+    }
+}
+
+
+
+    
+
+// }
