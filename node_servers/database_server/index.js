fec71ffceee7d70614e3ad0d719b7781212572ae
nikolasfil <nikolas13in@gmail.com>
2024-02-04 02:51:35 +0200

    checkpoint clearing clutter

--- node_servers/database_server/index.js
+++ node_servers/database_server/index.js
@@ -7,9 +7,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const sqliteStore = require('connect-sqlite3')(session) //store for session
 
-const login = require('./controllers/login.js');
 
 // Either use the port number from the environment or use 8080
+// Careful I may need to change this one 
 const port = process.env.PORT || 8080;
 
 // handle urls 
@@ -20,8 +20,6 @@ app.use(cookieParser());
 // using css,javascript, images and other public files 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-// app use model 
-// app.use('/model', express.static(`${__dirname}/model/`));
 app.use('/controllers', express.static(`${__dirname}/controllers/`));
 
 
@@ -39,30 +37,7 @@ app.use(session({
 }))
 
 
-// helpers
-// const hbs = expbs.create({
-//     defaultLayout: 'main',
-//     layoutsDir: path.join(__dirname, 'views/layouts'),
-//     partialsDir: path.join(__dirname, 'views/partials'),
-//     extname: '.hbs',
 
-//     // create custom helpers to be used in the handlebars files
-//     helpers: require('./controllers/helpers.js')
-// });
-
-// using the engine of handlebars
-// app.engine('.hbs', hbs.engine);
-// app.set('view engine', '.hbs');
-
-// app.use(login.alerting);
-
-// specifying the routes that the user can access
-// app.use(require('./routes/route_about.js'));
-// app.use(require('./routes/route_device_info.js'));
-// app.use(require('./routes/route_homepage.js'));
-// app.use(require('./routes/route_search.js'));
-// app.use(require('./routes/route_sign.js'));
-// app.use(require('./routes/route_user_profile.js'));
 
 app.use(require('./routes/route_database.js'));
 
