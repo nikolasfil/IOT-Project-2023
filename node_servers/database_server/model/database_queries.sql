b6cc41b4113bc3cb7d0d4d326e17e0519c87b59b
nikolasfil <nikolas13in@gmail.com>
2024-02-08 00:28:07 +0200

    working on sql queries

--- node_servers/database_server/model/database_queries.sql
+++ node_servers/database_server/model/database_queries.sql
@@ -24,6 +24,37 @@ WHERE
 type="tracker"
 
 
+-- Getting all the Tracked events of a tracker that is assigned on a user   
+
+SELECT 
+u_id, Assigned.device_id,
+date_received, date_returned
+FROM 
+USER 
+JOIN Assigned ON USER.u_id=Assigned.user_id
+JOIN DEVICE on DEVICE.d_id=Assigned.device_id
+JOIN Tracked on Assigned.device_id=Tracked.device_id
+WHERE
+date_tracking=date_received
+AND
+USER.u_id=?
+
+
+-- Getting all the Buttons of a tracker that is assigned to a user  
+
+
+SELECT 
+u_id, Assigned.device_id,
+date_received, date_returned
+FROM 
+USER 
+JOIN Assigned ON USER.u_id=Assigned.user_id
+JOIN DEVICE on DEVICE.d_id=Assigned.device_id
+JOIN Pressed on Assigned.device_id=Pressed.device_id
+WHERE
+date_pressed=date_received
+AND
+USER.u_id=?
 
 
 -- Getting all the Tracked events of a tracker that is assigned on a user 
@@ -44,3 +75,21 @@ USER.u_id=?
 -- Add the type of the device to check that it is tracker 
 
 -- ----------------------------
+
+
+-- Get all the dates the user had a Tracker assigned 
+
+SELECT
+device_id 
+FROM 
+Assigned 
+where 
+date_received
+and 
+user_id=?
+
+--- ----------------------------
+
+
+
+
