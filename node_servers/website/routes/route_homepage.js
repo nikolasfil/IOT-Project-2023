7cec2866ef933b024882bbd1b6c0268e59b629f6
nikolasfil <nikolas13in@gmail.com>
2024-02-12 00:02:45 +0200

    fixed the accessing rights of the homepage

--- node_servers/website/routes/route_homepage.js
+++ node_servers/website/routes/route_homepage.js
@@ -5,35 +5,28 @@ const database = require('../controllers/database.js');
 const login = require('../controllers/login.js');
 
 
-const homepage_route_list = [] 
-
+/**
+ * Middleware to get all active devices if the user is an admin
+ */
 getAllDevicesJson= (req, res, next) => {
-    // this is a static device list displaying 12 active trackers
+    if (res.locals.signedIn && res.locals.is_admin) {
         database.databaseRequest(link='/getAllDevicesJson',data = {status: 'active', type: 'Asset tracking', limit: 12, exclusively:true}, function (err, devices) {
-        if (err) {
-            console.log(err)
-            res.status(500).send('Internal Server Error')
-        } else {
-            res.locals.active_trackers = devices;
-            next();
-        }
-    })
-    
-}
-
-homepage_route_list.push(getAllDevicesJson)
-
-checkPrivileges = (req,res,next) => {
-    if (req.session.signedIn) {
-        res.locals.signedIn = true;
-        res.locals.user_id = req.session.userid;
+            if (err) {
+                console.log(err)
+                res.status(500).send('Internal Server Error')
+            } else {
+                res.locals.active_trackers = devices;
+                next();
+            }
+        })
+    } else {
+        next();
     }
-    next();
 }
 
 
 homepage_render = (req, res) => {
-    console.log(res.locals.active_trackers)
+    console.log(homepage_route_list)
     res.render('homepage', {
         style: 'index.css',
         title: 'Home',
@@ -41,11 +34,18 @@ homepage_render = (req, res) => {
     });
 }
 
+const homepage_route_list = [
+    login.checkAuthentication,
+    login.checkAdminRights,
+    getAllDevicesJson,
+    homepage_render
+] 
+
 
 /**
  * Router for homepage
  */
-router.get('/',homepage_route_list, homepage_render);
+router.get('/',homepage_route_list);
 
 
 
